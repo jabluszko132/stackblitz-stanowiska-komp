d56ce67ffc29bcf2109242b26515854576d3f5a3
jabluszko132 <123495481+jabluszko132@users.noreply.github.com>
2023-07-10 10:03:23 +0200

    added html file for root component

--- src/root/root/root.component.ts
+++ src/root/root/root.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './root.component.html',
   styleUrls: ['./root.component.css'],
+  standalone: true,
 })
 export class RootComponent implements OnInit {
   constructor() {}
